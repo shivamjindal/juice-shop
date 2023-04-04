b75a7a59291406d64edea0a0ca9ea03c3656db09
Shivam Jindal <jshivam21@gmail.com>
2023-04-04 11:25:04 -0400

    added vulns

--- routes/userProfile.ts
+++ routes/userProfile.ts
@@ -53,7 +53,7 @@ module.exports = function getUserProfile () {
           template = template.replace(/_primLight_/g, theme.primLight)
           template = template.replace(/_primDark_/g, theme.primDark)
           template = template.replace(/_logo_/g, utils.extractFilename(config.get('application.logo')))
-          // const fn = pug.compile(template)
+          const fn = pug.compile(template)
           const CSP = `img-src 'self' ${user?.profileImage}; script-src 'self' 'unsafe-eval' https://code.getmdl.io http://ajax.googleapis.com`
           challengeUtils.solveIf(challenges.usernameXssChallenge, () => { return user?.profileImage.match(/;[ ]*script-src(.)*'unsafe-inline'/g) !== null && utils.contains(username, '<script>alert(`xss`)</script>') })
 
@@ -61,7 +61,7 @@ module.exports = function getUserProfile () {
             'Content-Security-Policy': CSP
           })
 
-          // res.send(fn(user))
+          res.send(fn(user))
         }).catch((error: Error) => {
           next(error)
         })
